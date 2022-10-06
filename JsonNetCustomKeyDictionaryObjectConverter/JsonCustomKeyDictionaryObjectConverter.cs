5abc34bb762e4af026c4d9faa1a8fb09faa86fd5
VinzzF <funk.vin@t-online.de>
2022-10-06 14:45:16 +0200

    Initial code

---
+++ JsonNetCustomKeyDictionaryObjectConverter/JsonCustomKeyDictionaryObjectConverter.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JsonNetCustomKeyDictionaryObjectConverter
+{
+    /// <summary>
+    /// <para>Converter from and to <see cref="Dictionary{TKey, TValue}"/> types.</para>
+    /// <para>Works with non-string key types, as long as they can be serialized as plain strings (no complex structures).</para>
+    /// <para>Works for all dictionary types, without explicit generics binding.</para>
+    /// </summary>
+    public class JsonCustomKeyDictionaryObjectConverter : JsonConverter
+    {
+        /// <summary>
+        /// <para>Determines whether this instance can convert the specified object type.</para>
+        /// <para>true, if the specified object type is a <see cref="Dictionary{TKey, TValue}"/> and will be (de-)serialized</para>
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>true, if the specified object type is a <see cref="Dictionary{TKey, TValue}"/> and will be (de-)serialized</returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IDictionary).IsAssignableFrom(objectType)
+                || TypeImplementsGenericInterface(objectType, typeof(IDictionary<,>));
+        }
+
+        /// <summary>
+        /// Determines whether <paramref name="concreteType"/> implements <paramref name="interfaceType"/>
+        /// </summary>
+        /// <param name="concreteType">Type checked to contain interface type.</param>
+        /// <param name="interfaceType">Interface type.</param>
+        /// <returns>true, if <paramref name="concreteType"/> implements <paramref name="interfaceType"/></returns>
+        private static bool TypeImplementsGenericInterface(Type concreteType, Type interfaceType)
+        {
+            return concreteType.GetInterfaces()
+                   .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType);
+        }
+
+        /// <summary>
+        /// Acquires the generic Dictionary arguments via reflection.
+        /// </summary>
+        /// <param name="objectType">Type of the dictionary.</param>
+        /// <param name="dictionaryTypes">Output types of the generic arguments.</param>
+        /// <param name="isStringKey">Output whether the key is a <see cref="string"/> type.</param>
+        private static void GetDictGenericTypes(Type objectType, out Type[] dictionaryTypes, out bool isStringKey)
+        {
+            dictionaryTypes = objectType.GetGenericArguments();
+            if ((dictionaryTypes?.Length ?? 0) < 2)
+                throw new InvalidOperationException($"Deserializing Json dictionary with less than two types {objectType.Name}");
+            isStringKey = dictionaryTypes[0] == typeof(string);
+        }
+
+        /// <summary>
+        /// <para>Reads the JSON representation of the object.</para>
+        /// <para>Deseralizes each <see cref="KeyValuePair{TKey, TValue}"/> back into the resulting <see cref="Dictionary{TKey, TValue}"/>.</para>
+        /// </summary>
+        /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns><see cref="Dictionary{TKey, TValue}"/> consisting of each deserialized <see cref="KeyValuePair{TKey, TValue}"/>.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            // Aquire reflection info & create resulting dictionary:
+            GetDictGenericTypes(objectType, out Type[] dictionaryTypes, out bool isStringKey);
+            var res = Activator.CreateInstance(objectType) as IDictionary;
+
+            // Read each key-value-pair:
+            object key = null;
+            object value = null;
+
+            if (reader.TokenType != JsonToken.StartObject)
+                throw new JsonException("Json Dictionary is not represented as object");
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.EndObject)
+                {
+                    if (key != null || value != null) throw new JsonException($"Json Dictionary ended while still expecting key or value {key}, {value}");
+                    break;
+                }
+
+                if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    if (key != null) throw new JsonException($"Json Dictionary key {key} followed by another key, not value");
+                    if (reader.ValueType != typeof(string)) throw new JsonException($"Json Dictionary key {reader.Value} is no string type");
+                    key = isStringKey ? reader.Value : serializer.Deserialize(reader, dictionaryTypes[0]);
+                    if (value != null) throw new JsonException($"Json Dictionary key {key} read while value present");
+                }
+                else
+                {
+                    if (key == null) throw new JsonException($"Json Dictionary value read {reader.ReadAsString()} but no read before");
+                    if (value != null) throw new JsonException($"Json Dictionary value read {reader.ReadAsString()} but already has value {value}");
+                    value = serializer.Deserialize(reader, dictionaryTypes[1]);
+
+                    res.Add(key, value);
+                    key = null;
+                    value = null;
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// <para>Writes the JSON representation of the object.</para>
+        /// <para>Forces serialization of the key type, uses default serialization for the value type.</para>
+        /// </summary>
+        /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            // Aquire reflection info & get key-value-pairs:
+            Type type = value.GetType();
+            GetDictGenericTypes(type, out Type[] dictionaryTypes, out bool isStringKey);
+            IEnumerable keys = (IEnumerable)type.GetProperty("Keys").GetValue(value, null);
+            IEnumerable values = (IEnumerable)type.GetProperty("Values").GetValue(value, null);
+            IEnumerator valueEnumerator = values.GetEnumerator();
+
+            // Write each key-value-pair:
+            StringBuilder sb = new StringBuilder();
+            using (StringWriter tempWriter = new StringWriter(sb))
+            {
+                writer.WriteStartObject();
+                foreach (object key in keys)
+                {
+                    valueEnumerator.MoveNext();
+
+                    // convert key, force serialization of non-string keys
+                    string keyStr = null;
+                    if (isStringKey)
+                    {
+                        keyStr = (string)key;
+                    }
+                    else
+                    {
+                        sb.Clear();
+                        serializer.Serialize(tempWriter, key);
+                        keyStr = RemoveStripCapsulation(sb.ToString());
+                        // TO-DO: Validate key resolves to single string, no complex structure
+                    }
+                    writer.WritePropertyName(keyStr);
+
+                    // default serialize value
+                    serializer.Serialize(writer, valueEnumerator.Current);
+                }
+                writer.WriteEndObject();
+            }
+        }
+
+        /// <summary>
+        /// Removes string literal encapsulation if present on given string.
+        /// </summary>
+        /// <param name="str">String to remove encapsulation from.</param>
+        /// <returns>String without encapsulation.</returns>
+        private string RemoveStripCapsulation(string str)
+        {
+            if (str[0] == '\"' && str[str.Length-1] == '\"') return str.Substring(1, str.Length - 1);
+            return str;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,89 @@
+﻿using JsonNetCustomKeyDictionaryObjectConverter;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+// Non-string dictionary key type that serializes from and to a string
+class CustomKey
+{
+    public string Name { get; set; }
+}
+
+// Converter from and to string for custom key type
+class CustomKeyConverter : JsonConverter<CustomKey>
+{
+    public override CustomKey ReadJson(JsonReader reader, Type objectType, CustomKey existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        return new CustomKey { Name = (string)reader.Value };
+    }
+
+    public override void WriteJson(JsonWriter writer, CustomKey value, JsonSerializer serializer)
+    {
+        writer.WriteValue(value.Name);
+    }
+}
+
+// Value type that requires more complex (de-)serialization
+class ComplexVal
+{
+    public string Name { get; set; }
+
+    public int SomeInt { get; set; }
+
+    public string SomeOtherInfo { get; set; }
+}
+
+// Test object containing different nested dictionary types
+class Obj
+{
+    public Dictionary<string, string> DictPlain { get; set; } = new Dictionary<string, string>();
+    public Dictionary<CustomKey, string> DictSimple { get; set; } = new Dictionary<CustomKey, string>();
+    public Dictionary<CustomKey, ComplexVal> DictComplex { get; set; } = new Dictionary<CustomKey, ComplexVal>();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Test objects
+        var obj = new Obj();
+        obj.DictPlain.Add("key1", "val1");
+        obj.DictPlain.Add("key2", "val2");
+        obj.DictPlain.Add("key3", "val3");
+        obj.DictSimple.Add(new CustomKey { Name = "key1" }, "val1");
+        obj.DictSimple.Add(new CustomKey { Name = "key2" }, "val2");
+        obj.DictSimple.Add(new CustomKey { Name = "key3" }, "val3");
+        obj.DictComplex.Add(new CustomKey { Name = "key1" }, new ComplexVal { Name = "val1", SomeInt = 7, SomeOtherInfo = "bla" });
+        obj.DictComplex.Add(new CustomKey { Name = "key2" }, new ComplexVal { Name = "val2", SomeInt = 42, SomeOtherInfo = "lorem ipsum..." });
+        obj.DictComplex.Add(new CustomKey { Name = "key3" }, new ComplexVal { Name = "val3", SomeInt = 1337, SomeOtherInfo = "idk" });
+
+        // Serializer settings
+        var serializerSettings = new JsonSerializerSettings
+        {
+            Converters = new List<JsonConverter>
+            {
+                new JsonCustomKeyDictionaryObjectConverter(),
+                new CustomKeyConverter()
+            },
+            Formatting = Formatting.Indented
+        };
+
+        // Serialization
+        const string testfile = "test.json";
+        using (var sw = new StreamWriter(testfile))
+            sw.Write(JsonConvert.SerializeObject(obj, serializerSettings));
+
+        // Deserialization
+        Obj objDes = null;
+        using (var sr = new StreamReader(testfile))
+            objDes = JsonConvert.DeserializeObject<Obj>(sr.ReadToEnd(), serializerSettings);
+
+        // Result print
+        foreach (var kvp in objDes.DictPlain) Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+        foreach (var kvp in objDes.DictSimple) Console.WriteLine($"Key: {kvp.Key.Name}, Value: {kvp.Value}");
+        foreach (var kvp in objDes.DictComplex) Console.WriteLine($"Key: {kvp.Key.Name}, Value: {kvp.Value.Name}");
+        Console.Read();
+    }
+}
+
